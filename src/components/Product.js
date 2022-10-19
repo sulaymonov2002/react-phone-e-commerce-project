95e11f2ef28d1ae9460773fe32b9d110edbecd87
sulaymonov2002 <ssamir.web2002@gmail.com>
2022-10-19 18:40:29 +0500

    add new codes for functional

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { ProductConsumer } from "../context";
+import PropTypes from "prop-types";
 
 export default class Product extends Component {
   render() {
@@ -45,6 +46,17 @@ export default class Product extends Component {
     );
   }
 }
+
+Product.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number,
+    img: PropTypes.string,
+    title: PropTypes.string,
+    price: PropTypes.number,
+    inCart: PropTypes.bool,
+  }).isRequired,
+};
+
 const ProductWrapper = styled.div`
   .card {
     border-color: transparent;
@@ -84,5 +96,13 @@ const ProductWrapper = styled.div`
     font-size: 1.4rem;
     border-radius: 0.5rem 0 0 0;
     transform: translate(100%, 100%);
+    transition: all 1s linear;
+  }
+  .img-container:hover .cart-btn {
+    transform: translate(0, 0);
+  }
+  .cart-btn:hover {
+    color: var(--mainBlue);
+    cursor: pointer;
   }
 `;
--- src/context.js
+++ src/context.js
@@ -17,6 +17,17 @@ class ProductProvider extends Component {
     console.log("hello from add to cart");
   };
 
+  tester = () => {
+    console.log("State products : ", this.state.products[0].inCart);
+    console.log("Date products : ", storeProducts[0].inCart);
+
+    const tempProducts = [...this.state.products];
+    tempProducts[0].inCart = true;
+    this.setState(() => {
+      return { products: tempProducts };
+    });
+  };
+
   render() {
     return (
       <ProductContact.Provider
