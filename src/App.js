674d8bbde9cb046bcf8b8e3b33cf84e9d1da12d8
sulaymonov2002 <ssamir.web2002@gmail.com>
2022-10-20 17:31:10 +0500

    add new codes for modal component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ProductList from "./components/ProductList";
 import Details from "./components/Details";
 import Cart from "./components/Cart";
 import Default from "./components/Default";
+import Modal from "./components/Modal";
 
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
@@ -13,13 +14,14 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Navbar /> 
+        <Navbar />
         <Switch>
-          <Route exact  path="/" component={ProductList} />
+          <Route exact path="/" component={ProductList} />
           <Route path="/details" component={Details} />
           <Route path="/cart" component={Cart} />
           <Route component={Default} />
         </Switch>
+        <Modal />
       </React.Fragment>
     );
   }
